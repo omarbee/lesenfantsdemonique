35c5dc8f0bc6692b46ab7fe340c07fab785cc92c
JACOB <oaaiachi@adneom.com>
2018-11-14 14:13:44 +0100

    content findbyid delete modify

--- api/controllers/contentController.js
+++ api/controllers/contentController.js
@@ -3,8 +3,14 @@ const { sendError } = require('../functions/sendFunction');
 
 
 exports.getContent = function(req, res, next) {
-    contentModel.find({}, function(err, contents) {
+    let param = {};
+    if (req.params.idcontent) {
+        param = { _id: req.params.idcontent };
+    }
+
+    contentModel.find(param, function(err, users) {
         if (err) return sendError(res, err);
+        if (contents.length === 0) return sendError(res, 'Content not found', 404);
         res.send(contents);
     });
 };
@@ -31,6 +37,15 @@ exports.addContent = function(req, res, next) {
 
     
 };
+
+exports.modifyContent = function(req, res, next) {
+    if (!req.body.title) return sendError(res, 'title is required');
+    if (!req.body.text) return sendError(res, 'text is required');
+    if (!req.body.type) return sendError(res, 'type is required');
+
+    
+};
+
 exports.deleteContent = function(req, res, next) {
     contentModel.findByIdAndRemove(req.params.idcontent, function(err, user) {
         if (err) return sendError(res, err);
--- api/routes/contents.js
+++ api/routes/contents.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const {getContent, addContent, deleteContent} = require('../controllers/contentController')
+const {getContent, addContent, modifyContent, deleteContent} = require('../controllers/contentController')
 
 
 router.get('/',getContent);
+router.get('/:idcontent',getContent);
 router.post('/',addContent);
+router.put('/:idcontent',modifyContent);
 router.delete('/:idcontent',deleteContent);
 
 module.exports = router;
